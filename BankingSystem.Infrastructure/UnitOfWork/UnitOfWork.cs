e6bb2cf33b4e86141b8e27a46b81eed9aebfdf66
NoobMaster69V10 <giorgi.ubilava.1@btu.edu.ge>
2025-04-17 09:27:40 +0400

    fix: unitofwork

--- BankingSystem.Infrastructure/UnitOfWork/UnitOfWork.cs
+++ BankingSystem.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -10,31 +10,24 @@ public class UnitOfWork : IUnitOfWork
     private IDbTransaction? _transaction;
     private bool _disposed;
 
-    private readonly Lazy<IRefreshTokenRepository> _refreshTokenRepository;
-    private readonly Lazy<IPersonRepository> _personRepository;
-    private readonly Lazy<IBankTransactionRepository> _bankTransactionRepository;
-    private readonly Lazy<IBankCardRepository> _bankCardRepository;
-    private readonly Lazy<IBankAccountRepository> _bankAccountRepository;
-    private readonly Lazy<IBankReportRepository> _bankReportRepository;
-
-    public IRefreshTokenRepository RefreshTokenRepository => _refreshTokenRepository.Value;
-    public IPersonRepository PersonRepository => _personRepository.Value;
-    public IBankTransactionRepository BankTransactionRepository => _bankTransactionRepository.Value;
-    public IBankCardRepository BankCardRepository => _bankCardRepository.Value;
-    public IBankAccountRepository BankAccountRepository => _bankAccountRepository.Value;
-    public IBankReportRepository BankReportRepository => _bankReportRepository.Value;
+    public IPersonRepository PersonRepository { get; }
+    public IBankTransactionRepository BankTransactionRepository { get; }
+    public IBankCardRepository BankCardRepository { get; }
+    public IBankAccountRepository BankAccountRepository { get; }
+    public IRefreshTokenRepository RefreshTokenRepository { get; }
+    public IBankReportRepository BankReportRepository { get; }
 
     public UnitOfWork(IDbConnection connection, IPersonRepository personRepository,
         IBankTransactionRepository transactionRepository, IBankCardRepository bankCardRepository,
         IBankAccountRepository bankAccountRepository, IBankReportRepository bankReportRepository, IRefreshTokenRepository refreshTokenRepository)
     {
         _connection = connection;
-        _personRepository = new Lazy<IPersonRepository>(() => personRepository);
-        _bankTransactionRepository = new Lazy<IBankTransactionRepository>(() => transactionRepository);
-        _bankCardRepository = new Lazy<IBankCardRepository>(() => bankCardRepository);
-        _bankAccountRepository = new Lazy<IBankAccountRepository>(() => bankAccountRepository);
-        _bankReportRepository = new Lazy<IBankReportRepository>(() => bankReportRepository);
-        _refreshTokenRepository = new Lazy<IRefreshTokenRepository>(() => refreshTokenRepository);
+        PersonRepository = personRepository;
+        BankTransactionRepository = transactionRepository;
+        BankCardRepository = bankCardRepository;
+        BankAccountRepository = bankAccountRepository;
+        BankReportRepository = bankReportRepository;
+        RefreshTokenRepository = refreshTokenRepository;
         _connection.Open();
     }
 
